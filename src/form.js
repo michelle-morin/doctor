a1a7a251a3cd52e1f0af1835250cf1206969a6b7
Michelle Morin <morin2m@gmail.com>
2020-02-14 15:45:34 -0800

    add logic for validating user input

--- src/form.js
+++ src/form.js
@@ -5,11 +5,27 @@ export function initializePage() {
   $("form#name-search").submit(function(event) {
     event.preventDefault();
     const inputName = $("input#name").val();
-    showResultsByName(inputName);
+    if (parseFloat(inputName)) {
+      $("h3.if-results").hide();
+      $("#search-inputs").hide();
+      $("h3#numeric").html("Please enter a name.");
+      $("#output").show();
+    } else {
+      $("h3#numeric").html("");
+      showResultsByName(inputName);
+    }
   });
   $("form#issue-search").submit(function(event) {
     event.preventDefault();
     const inputIssue = $("input#issue").val();
-    showResultsByIssue(inputIssue);
+    if (parseFloat(inputIssue)) {
+      $("h3.if-results").hide();
+      $("#search-inputs").hide();
+      $("h3#numeric").html("Please enter a word containing only alphabetical characters, or multiple words each separated by a comma.");
+      $("#output").show();
+    } else {
+      $("h3#numeric").html("");
+      showResultsByIssue(inputIssue);
+    }
   });
 }
