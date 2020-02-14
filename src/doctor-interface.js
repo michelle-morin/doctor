3b3663e1849d7735de51246b069c200b8f86f128
Michelle Morin <morin2m@gmail.com>
2020-02-14 12:16:43 -0800

    update logic to determine doctor website and display doctor website, if available in api response

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -17,6 +17,7 @@ export function showResultsByName() {
       $("#output").append(`<h3>Unfortunately, there are no Portland, OR area doctors matching your search criteria. <a href='index.html'>Click here</a> to try another search.</h3>`);
     } else if (nameResponse.data.length > 0) {
       $("h3#if-results").show();
+      console.log(nameResponse);
       nameResponse.data.forEach(function(doctor) {
         let portlandPractice = {};
         if (doctor.practices.length === 1) {
@@ -40,9 +41,13 @@ export function showResultsByName() {
         } else {
           secondLineAddress = "";
         }
-        let website;
-        // need logic to determine whether or not doctor has a website.
-        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${website}</li></ul></li>`);
+        let resultWebsite;
+        if (portlandPractice.website) {
+          resultWebsite = portlandPractice.website;
+        } else {
+          resultWebsite = "not available";
+        }
+        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
       });
       $("#output").append("<a href='index.html'>Click here to search again</a>");
     }
@@ -89,9 +94,13 @@ export function showResultsByIssue() {
         } else {
           secondLineAddress = "";
         }
-        let website;
-        // need logic to determine whether or not doctor has a website.
-        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${website}</li></ul></li>`);
+        let resultWebsite;
+        if (portlandPractice.website) {
+          resultWebsite = portlandPractice.website;
+        } else {
+          resultWebsite = "not available";
+        }
+        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
       });
       $("#output").append("<a href='index.html'>Click here to search again</a>");
     }
