b06588bc02e57fbfdcfcf4f18d71f9fa9455df5d
Michelle Morin <morin2m@gmail.com>
2020-02-14 11:50:47 -0800

    update getElementsByName and getElementsByIssue to show output div

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
       } else if (nameResponse.data.length === 0) {
         $("#output").append(`<h3>Unfortunately, there are no Portland, OR area doctors matching your search criteria. <a href='index.html'>Click here</a> to try another search.</h3>`);
       } else if (nameResponse.data.length > 0) {
+        $("h3#if-results").show();
         console.log(nameResponse);
         nameResponse.data.forEach(function(doctor) {
           let portlandPractice = {};
@@ -52,8 +53,8 @@ $(document).ready(function() {
           $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${website}</li></ul></li>`);
         });
         $("#output").append("<a href='index.html'>Click here to search again</a>");
-        $("#output").show();
       }
+      $("#output").show();
     };
   });
 
@@ -75,6 +76,7 @@ $(document).ready(function() {
       } else if (issueResponse.data.length === 0) {
         $("#output").append(`<h3>Unfortunately, there are no Portland, OR area doctors matching your search criteria. <a href='index.html'>Click here</a> to try another search.</h3>`);
       } else if (issueResponse.data.length > 0) {
+        $("h3#if-results").show();
         console.log(issueResponse);
         issueResponse.data.forEach(function(doctor) {
           let portlandPractice = {};
@@ -104,8 +106,8 @@ $(document).ready(function() {
           $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${website}</li></ul></li>`);
         });
         $("#output").append("<a href='index.html'>Click here to search again</a>");
-        $("#output").show();
       }
+      $("#output").show();
     };
   });
 });
