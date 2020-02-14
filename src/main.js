2a2dcf2e538414fe3d8aaa25233d4f1db9b17bca
Michelle Morin <morin2m@gmail.com>
2020-02-14 08:50:05 -0800

    add import statements for jquery, bootstrap, and custom styles, and add initial UI logic for obtaining form input values

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
+
+$(document).ready(function() {
+  $("form#name-search").submit(function(event) {
+    event.preventDefault();
+    const inputName = $("input#name").val();
+
+  });
+
+  $("form#issue-search").submit(function(event) {
+    event.preventDefault();
+    const inputIssue = $("input#issue").val();
+
+  });
+});
