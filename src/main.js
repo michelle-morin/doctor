fb185f80c364952a7e55b134ee718001b0a918b9
Michelle Morin <morin2m@gmail.com>
2020-02-14 08:58:59 -0800

    add initial UI logic for API calls

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { DoctorService } from './doctor-service';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,11 +9,29 @@ $(document).ready(function() {
     event.preventDefault();
     const inputName = $("input#name").val();
 
+    (async () => {
+      let doctorNameService = new DoctorService();
+      const nameResponse = await doctorNameService.getDoctorsByName(inputName);
+      getElementsByName(nameResponse);
+    })();
+
+    const getElementsByName = function(nameResponse) {
+      // add logic for function here.
+    };
   });
 
   $("form#issue-search").submit(function(event) {
     event.preventDefault();
     const inputIssue = $("input#issue").val();
 
+    (async () => {
+      let doctorIssueService = new DoctorService();
+      const issueResponse = await doctorIssueService.getDoctorsByIssue(inputIssue);
+      getElementsByIssue(issueResponse);
+    })();
+
+    const getElementsByIssue = function(issueResponse) {
+      // add logic for function here.
+    };
   });
 });
