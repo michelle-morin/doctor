4ac98250bb1066bf4554c2c6ec4f034dafb64f10
Michelle Morin <morin2m@gmail.com>
2020-02-14 10:46:42 -0800

    update api call urls to return query results in ascending order of distance from portland, or

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DoctorService {
   async getDoctorsByName(name) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=25&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&sort=distance-asc&skip=0&limit=25&user_key=${process.env.API_KEY}`);
       if (response.status != 200 || response.ok != true) {
         return false;
       } else {
@@ -14,7 +14,7 @@ export class DoctorService {
   }
   async getDoctorsByIssue(issue) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=50&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&sort=distance-asc&skip=0&limit=25&user_key=${process.env.API_KEY}`);
       if (response.status != 200 || response.ok != true) {
         return false;
       } else {
