dca554c27dc241c3fca5a6b5750a4d28723df122
Michelle Morin <morin2m@gmail.com>
2020-02-14 08:58:39 -0800

    add business logic for async API calls

---
+++ src/doctor-service.js
@@ -0,0 +1,28 @@
+export class DoctorService {
+  async getDoctorsByName(name) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      if (response.status != 200 || response.ok != true) {
+        return false;
+      } else {
+        let jsonifiedResponse = await response.json();
+        return jsonifiedResponse;
+      }
+    } catch(error) {
+      return false;
+    }
+  }
+  async getDoctorsByIssue(issue) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/conditions?fields=${issue}&user_key=${process.env.API_KEY}`);
+      if (repsonse.status != 200 || response.ok != true) {
+        return false;
+      } else {
+        let jsonifiedResponse = response.json();
+        return jsonifiedResponse;
+      }
+    } catch(error) {
+      return false;
+    }
+  }
+}
