7c3f23510a80abf36aa01bacc14c5b4ddab3de48
Michelle Morin <morin2m@gmail.com>
2020-02-14 12:30:49 -0800

    add strong tag around doctor first and last names

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -17,7 +17,6 @@ export function showResultsByName(inputName) {
       $("#output").append(`<h3>Unfortunately, there are no Portland, OR area doctors matching your search criteria. <a href='index.html'>Click here</a> to try another search.</h3>`);
     } else if (nameResponse.data.length > 0) {
       $("h3#if-results").show();
-      console.log(nameResponse);
       nameResponse.data.forEach(function(doctor) {
         let portlandPractice = {};
         if (doctor.practices.length === 1) {
@@ -47,7 +46,7 @@ export function showResultsByName(inputName) {
         } else {
           resultWebsite = "not available";
         }
-        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
+        $("ol#results").append(`<li><strong>${doctor.profile.first_name} ${doctor.profile.last_name}</strong><ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
       });
       $("#output").append("<a href='index.html'>Click here to search again</a>");
     }
@@ -100,7 +99,7 @@ export function showResultsByIssue(inputIssue) {
         } else {
           resultWebsite = "not available";
         }
-        $("ol#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name}<ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
+        $("ol#results").append(`<li><strong>${doctor.profile.first_name} ${doctor.profile.last_name}</strong><ul><li>Clinic address: ${portlandPractice.visit_address.street} ${secondLineAddress} ${portlandPractice.visit_address.city}, ${portlandPractice.visit_address.state} ${portlandPractice.visit_address.zip}</li><li>${acceptingNewPatients}</li><li>Phone Number: ${portlandPractice.phones[0].number}</li><li>Website: ${resultWebsite}</li></ul></li>`);
       });
       $("#output").append("<a href='index.html'>Click here to search again</a>");
     }
